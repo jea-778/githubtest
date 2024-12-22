4cb87564600fc5a6dab70a56449420fe0cf1e8c7
Rizanoor <rizanoorfauzan123@gmail.com>
2024-12-22 13:50:20 +0700

    feat: chagnge title h1

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>TUTORIAL GITHUB</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
